a9e350f83e796d6e822a1471dd083e816cf2d239
rishuraj-7 <rishuraj7clg@gmail.com>
2026-09-11 18:42:04 +0530

    Time: 1 ms (100%), Space: 43.8 MB (96.13%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,22 +1,26 @@
-class Solution {
-    public int totalNumbers(int[] d) {
-       Set<Integer> set=new HashSet<>();
-        int n=d.length;
-        
-        for(int i=0;i<n;i++){
-            if(d[i]==0) continue;
-            for(int j=0;j<n;j++){
-                if(i==j) continue;
-                for(int k=0;k<n;k++){
-                    if(k==i||k==j) continue;
-                    if(d[k]%2!=0) continue;
-                    int no=(d[i]*100)+(d[j]*10)+(d[k]);
-                     set.add(no);
-                }
-                
-            }
+class Solution{
+    public int totalNumbers(int[] digits){
+        int[] freq=new int[10];
+        for(int d:digits){
+            freq[d]++;
         }
-        return set.size();
+        int count=0;
+        for(int i=1;i<=9;i++){
+            if(freq[i]==0)continue;
+            freq[i]--;
+            for(int j=0;j<=9;j++){
+                if(freq[j]==0)continue;
+                freq[j]--;
+                for(int k=0;k<=8;k+=2){
+                    if(freq[k]>0){
+                        count++;
+                    }
+                }
+                freq[j]++;
+            }
+            freq[i]++;
+        }
 
+        return count;
     }
 }
