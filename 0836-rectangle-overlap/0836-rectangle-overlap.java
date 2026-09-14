96464261ab1d4ba6dc2a9dfefb057a225bdf8d9a
Rishu Raj <rishuraj7clg@gmail.com>
2026-09-14 16:38:35 +0530

    Time: 0 ms (100%), Space: 42.7 MB (33.3%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isRectangleOverlap(int[] r1, int[] r2) {
+        if(r1[2]<=r2[0] || r1[0]>=r2[2] || r1[3]<=r2[1]|| r1[1]>=r2[3]){
+            return false;
+        }
+        return true;
+    }
+}
